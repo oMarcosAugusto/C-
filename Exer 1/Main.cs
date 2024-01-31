38db2ecefe9309a1b6584bbe069952127271b74a
oMarcosAugusto <marcoosaugusto@hotmail.com>
2024-01-31 07:57:28 +0000

    Exercício 1 concluído! || Exercise 1 completed!

---
+++ Exer 1/Main.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+    static void Main() {
+        Console.WriteLine("Digite um numero: ");
+        string res = Console.ReadLine();
+        Console.WriteLine("O numero informado foi o " + res);
+    }
+}
